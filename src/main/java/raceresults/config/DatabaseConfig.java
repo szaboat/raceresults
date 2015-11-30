f14d1a4f4962f6f0cd3ea7c83d27f64761f6456f
Denes Arvay <denes.arvay@prezi.com>
2015-11-30 20:54:01 +0100

    fix error if db url doesn't contain user and password

--- src/main/java/raceresults/config/DatabaseConfig.java
+++ src/main/java/raceresults/config/DatabaseConfig.java
@@ -15,14 +15,17 @@ public class DatabaseConfig {
     public DataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+
+        if (dbUri.getUserInfo() != null) {
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+
+            basicDataSource.setUsername(username);
+            basicDataSource.setPassword(password);
+        }
 
         return basicDataSource;
     }

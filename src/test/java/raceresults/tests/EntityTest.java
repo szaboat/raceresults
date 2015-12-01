eba71137c88c50264cb922f7f945739d819f79f2
Denes Arvay <denes.arvay@prezi.com>
2015-12-01 12:05:58 +0100

    fix tests

--- src/test/java/raceresults/tests/EntityTest.java
+++ src/test/java/raceresults/tests/EntityTest.java
@@ -7,10 +7,12 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import raceresults.Application;
+import raceresults.TestApplication;
 import raceresults.TestPersistenceContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { TestPersistenceContext.class })
+@ContextConfiguration(classes = { TestPersistenceContext.class, TestApplication.class})
 @TestExecutionListeners({
 		DependencyInjectionTestExecutionListener.class,
 		DirtiesContextTestExecutionListener.class,
